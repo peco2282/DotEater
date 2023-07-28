45ed9e30a0ef376ec90047d3dbf4138b9f345f8f
peco2282 <87461070+peco2282@users.noreply.github.com>
2023-07-28 19:33:19 +0900

    add .htaccess

--- Assets/Scripts/BiggerPlayer.cs
+++ Assets/Scripts/BiggerPlayer.cs
@@ -1,24 +1,39 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BiggerPlayer : MonoBehaviour
 {
-  private int point = 0;
+  private int _point = 0;
+  public GameObject textObject;
+  public GameObject dotObject;
+  private TextMeshProUGUI _text;
+  private DotController _dotController;
+
+  private void Start()
+  {
+    _text          = textObject.GetComponent<TextMeshProUGUI>();
+    _dotController = dotObject.GetComponent<DotController>();
+  }
+
   private void OnTriggerEnter(Collider other)
   {
     if (other.CompareTag("Dot"))
     {
       Destroy(other.gameObject);
-      point++;
+      _point++;
+      _text.text = "Score: " + _point;
     }
     if (other.CompareTag("Enemy"))
     {
       SceneManager.LoadScene("Lose");
     }
-    Debug.Log(point);
-    if (Input.GetKey(KeyCode.I))
+
+    if (_point == _dotController.GetChildLength())
     {
-      
+      SceneManager.LoadScene("Win", LoadSceneMode.Single);
     }
   }
 }
--- Assets/Scripts/DotController.cs
+++ Assets/Scripts/DotController.cs
@@ -1,16 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DotController : MonoBehaviour
 {
-  // Start is called before the first frame update
-  void Start()
+  public float top, bottom;
+  private bool _toUp = true;
+
+
+  private GameObject[] _objects;
+
+  public int GetChildLength()
   {
+    return _objects.Length;
+  }
+
+  private void Start()
+  {
+    _objects = new GameObject[transform.childCount];
+
+    for (int i = 0; i < transform.childCount; i++)
+    {
+      _objects[i] = transform.GetChild(i).gameObject;
+    }
   }
 
   // Update is called once per frame
   void Update()
   {
+    if (transform.position.y > top)
+    {
+      _toUp = false;
+    }
+
+    if (transform.position.y < bottom)
+    {
+      _toUp = true;
+    }
+
+    transform.position += new Vector3(0, (float)((_toUp ? 1 : -1) * 0.02), 0);
+    // transform.Translate(transform.position.x, transform.position.y + (float)((_toUp ? 1 : -1) * 0.2), transform.position.z);
   }
 }
